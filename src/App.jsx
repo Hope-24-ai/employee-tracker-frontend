5b9634c091fa83ff5f8b754766eee93c509ca830
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-06-24 16:02:51 +0300

    Initial commit for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,44 @@ function App() {
 }
 
 export default App
+=======
+import React, { useState } from 'react';
+import EmployeeDashboard from './components/EmployeeDashboard';
+import LoginScreen from './components/LoginScreen';
+import './App.css'; 
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentEmployeeId, setCurrentEmployeeId] = useState(null);
+
+  const handleLogin = (employeeId) => {
+    
+    if (employeeId) {
+      setCurrentEmployeeId(employeeId);
+      setIsLoggedIn(true);
+    } else {
+      alert('Please enter your Employee ID.');
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setCurrentEmployeeId(null);
+  };
+
+  return (
+    <div className="app-container">
+      {!isLoggedIn ? (
+        <LoginScreen onLogin={handleLogin} />
+      ) : (
+        <EmployeeDashboard
+          employeeId={currentEmployeeId}
+          onLogout={handleLogout}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 53fcf48 (Initial commit for frontend)
