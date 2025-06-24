e3ce50277092255bcc1f3c55f9b4a0182000dbd5
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-06-24 18:11:48 +0300

    push frontend code

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
 import React, { useState } from 'react';
 import EmployeeDashboard from './components/EmployeeDashboard';
 import LoginScreen from './components/LoginScreen';
 import './App.css'; 
-function App() {
 
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentEmployeeId, setCurrentEmployeeId] = useState(null);
 
   const handleLogin = (employeeId) => {
-    
     if (employeeId) {
       setCurrentEmployeeId(employeeId);
       setIsLoggedIn(true);
@@ -74,4 +36,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 53fcf48 (Initial commit for frontend)
